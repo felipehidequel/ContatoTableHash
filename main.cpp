f6e8ba273e1fa1db4fff68ba375f00e4555432e1
Felipe Hidequel <92414576+felipehidequel@users.noreply.github.com>
2024-03-17 16:58:03 +0000

    moved code files to src/

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
-#include "contato.cpp"
-#include "hashTable.cpp"
+#include "./src/contato.cpp"
+#include "./src/hashTable.cpp"
 
 int main(){
-    HashTable tabela;
-    Contato c1;
-    c1.setNome("Joao");
-    c1.setEmail("felipehidequel@gmail.com");
-    c1.setTelefone("999999999");
-    tabela.insere(c1.getNome(), c1);
-    tabela.insere(c1.getNome(), c1);
+HashTable tabela;
+Contato c1;
+Contato c2;
+c1.setNome("Joao");
+c1.setEmail("felipehidequel@gmail.com");
+c1.setTelefone("999999999");
+c2.setNome("Joao batista");
+c2.setEmail("hidequel@gmail.com");
+c2.setTelefone("3423546346");
+tabela.insere(c1.getNome(), c1);
+tabela.remove(c1.getNome());
+tabela.insere(c1.getNome(), c1);
+tabela.insere(c2.getNome(), c2);
 }
